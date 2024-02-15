cf8b64a77fb01e400657c45c0c8a8154be7fe6a2
5ouma <101255979+5ouma@users.noreply.github.com>
2024-02-15 17:24:53 +0900

    feat(flag): Get flags where to read/save from/to
    Users can change file name and output directory.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import { parseArgs } from "parse_args";
+import { resolve } from "path";
 import { readTOML, writeXML } from "./libs/mod.ts";
 import { Lists } from "./types/mod.ts";
 
+const flags = parseArgs(Deno.args, {
+  string: ["feeds", "output"],
+  default: {
+    feeds: "./feeds.toml",
+    output: "./outputs",
+  },
+});
+flags.feeds = resolve(flags.feeds);
+flags.output = resolve(flags.output);
+
 try {
-  const feeds: Lists = await readTOML("feeds.toml");
-  await writeXML(feeds, "outputs");
+  const feeds: Lists = await readTOML(flags.feeds);
+  await writeXML(feeds, flags.output);
 } catch (error) {
   console.error(`🚨 ${error.message}`);
   Deno.exit(1);
