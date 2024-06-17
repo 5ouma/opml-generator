e9237dc3102ce841353bdca318894ec42cf0c507
5ouma <101255979+5ouma@users.noreply.github.com>
2024-06-17 20:35:30 +0900

    test(step): Use step from `Deno.TestContext`
    It makes the result clean and readable.

--- test/libs/sites_test.ts
+++ test/libs/sites_test.ts
@@ -2,44 +2,50 @@ import { assertEquals } from "@std/assert";
 import { transcodeXmlUrl } from "../../src/libs/sites.ts";
 import type { Feed } from "../../src/types/mod.ts";
 
-Deno.test("Get XML URL (Site)", () => {
-  const feed: Feed = {
-    title: "feed title",
-    type: "bluesky",
-    id: "username",
-  };
-  assertEquals(
-    transcodeXmlUrl(feed.title, feed.type, feed.id),
-    new URL("https://bsky.app/profile/username/rss"),
-  );
-});
+Deno.test("Get XML URL", async (t: Deno.TestContext) => {
+  await t.step("site", () => {
+    const feed: Feed = {
+      title: "feed title",
+      type: "bluesky",
+      id: "username",
+    };
 
-Deno.test("Get XML URL (type not set)", () => {
-  const feed: Feed = { title: "feed title", id: "username" };
-  try {
-    transcodeXmlUrl(feed.title, undefined, feed.id);
-  } catch (error) {
-    assertEquals(error.message, `Parameter not set: "type" of "${feed.title}"`);
-  }
-});
-
-Deno.test("Get XML URL (id not set)", () => {
-  const feed: Feed = { title: "feed title", type: "bluesky" };
-  try {
-    transcodeXmlUrl(feed.title, feed.type, undefined);
-  } catch (error) {
-    assertEquals(error.message, `Parameter not set: "id" of "${feed.title}"`);
-  }
-});
-
-Deno.test("Get XML URL (Site not found)", () => {
-  const feed: Feed = { title: "feed title", type: "unknown", id: "username" };
-  try {
-    transcodeXmlUrl(feed.title, feed.type, feed.id);
-  } catch (error) {
     assertEquals(
-      error.message,
-      `Site not found: "${feed.type}" of "${feed.title}"`,
+      transcodeXmlUrl(feed.title, feed.type, feed.id),
+      new URL("https://bsky.app/profile/username/rss"),
     );
-  }
+  });
+
+  await t.step("type not set", () => {
+    const feed: Feed = { title: "feed title", id: "username" };
+    try {
+      transcodeXmlUrl(feed.title, undefined, feed.id);
+    } catch (error) {
+      assertEquals(
+        error.message,
+        `parameter not set: "type" of "${feed.title}"`,
+      );
+    }
+  });
+
+  await t.step("id not set", () => {
+    const feed: Feed = { title: "feed title", type: "bluesky" };
+    try {
+      transcodeXmlUrl(feed.title, feed.type, undefined);
+    } catch (error) {
+      assertEquals(error.message, `parameter not set: "id" of "${feed.title}"`);
+    }
+  });
+
+  await t.step("site not found", () => {
+    const feed: Feed = { title: "feed title", type: "unknown", id: "username" };
+    try {
+      transcodeXmlUrl(feed.title, feed.type, feed.id);
+    } catch (error) {
+      assertEquals(
+        error.message,
+        `site not found: "${feed.type}" of "${feed.title}"`,
+      );
+    }
+  });
 });
