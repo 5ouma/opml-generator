3d21b3290a086902ecafe25812fae80a8da081bc
Souma <101255979+5ouma@users.noreply.github.com>
2024-07-06 23:16:29 +0900

    feat(sites): New sites are added (#93)
    Add Nitter and Zenn for ease.

--- src/libs/sites.ts
+++ src/libs/sites.ts
@@ -1,10 +1,19 @@
+import { join } from "@std/url";
 import type { site } from "@5ouma/opml-generator/types";
 
 const sites: site[] = [
   { type: "bluesky", url: new URL("https://bsky.app/profile/{id}/rss") },
+  {
+    type: "nitter",
+    url: join(
+      new URL(`https://${Deno.env.get("NITTER_DOMAIN") ?? "cdn.xcancel.com"}`),
+      "/search/rss?f=tweets&q={id}",
+    ),
+  },
   { type: "note", url: new URL("https://note.com/{id}/rss") },
   { type: "reddit", url: new URL("https://www.reddit.com/r/{id}/.rss") },
   { type: "sizu.me", url: new URL("https://sizu.me/{id}/rss") },
+  { type: "zenn", url: new URL("https://zenn.dev/{id}/feed") },
 ];
 
 export function transcodeXmlUrl(
@@ -19,5 +28,7 @@ export function transcodeXmlUrl(
     .find((site: site) => site.type === type)?.url;
   if (!url) throw new Error(`site not found: "${type}" of "${title}"`);
 
-  return new URL(url.href.replace(encodeURI("{id}"), id));
+  return new URL(
+    url.href.replace(encodeURI("{id}"), id).replaceAll("%3F", "?"),
+  );
 }
