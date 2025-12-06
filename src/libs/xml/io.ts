2e779eab3e4c012273db23d3480b8068d59e437c
Souma <101255979+5ouma@users.noreply.github.com>
2025-12-06 14:43:27 +0900

    fix(libs): Await all maps of XML IO (#355)
    It occurs a memory leak because of non-awaiting.

--- src/libs/xml/io.ts
+++ src/libs/xml/io.ts
@@ -35,12 +35,12 @@ import type { List, Lists } from "../types.ts";
  */
 export async function write(feeds: Lists, dir: string): Promise<void> {
   await Deno.mkdir(dir, { recursive: true });
-  feeds.lists.map(async (list: List) => {
+  await Promise.all(feeds.lists.map(async (list: List) => {
     const file: string = format({
       dir: dir,
       name: paramCase(list.name),
       ext: ".xml",
     });
     await Deno.writeTextFile(file, convert(list));
-  });
+  }));
 }
